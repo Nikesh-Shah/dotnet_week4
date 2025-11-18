346f982026df1c807d8a5980bcd94da71c82b060
Nikesh Shah <156276029+Nikesh-Shah@users.noreply.github.com>
2025-11-18 09:36:45 +0545

    task_3 completed

---
+++ Task_3/ParameterDemo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    public class ParameterDemo
+    {
+        public int Increase(ref int num)
+        {
+            num = 20;
+            num += 10;
+            return num;
+        }
+        public String GetFullName(out String fullName)
+        {
+             fullName = "ROhan poudel";
+            return fullName;
+        }
+
+        public int SumAll(params int[] nums)
+        {
+            return nums.Sum();
+        }
+    }
+}
---
+++ Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = 5;
+            string name="Nikesh";
+            ParameterDemo parameterDemo = new ParameterDemo();
+            Console.WriteLine(parameterDemo.Increase(ref number));
+            Console.WriteLine(parameterDemo.GetFullName(out name));
+            Console.WriteLine(parameterDemo.SumAll(1, 2, 3, 4, 25));
+        }
+    }
+}
