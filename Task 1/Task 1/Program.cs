77776491e97a1b0686377bf770001c34fe7493a6
Nikesh Shah <156276029+Nikesh-Shah@users.noreply.github.com>
2025-11-18 08:53:16 +0545

    task 1 completed

---
+++ Task 1/Task 1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("ROhan", 22, "15th Grade");
+            Student student2 = new Student("Madan", 24, "15th Grade");
+            Console.WriteLine($"School:{Student.schoolName} Student 1: Name={student1.Name}, Age={student1.Age}, Grade={student1.Grade}");
+            Console.WriteLine($"School:{Student.schoolName}Student 2: Name={student2.Name}, Age={student2.Age}, Grade={student2.Grade}");
+        }
+    }
+
+
+}
---
+++ Task 1/Task 1/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class Student
+    {
+        public static string schoolName = "Ithari International College";
+        public string Name;
+        public int Age;
+        public string Grade;
+        public Student(string name, int age, string grade)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Grade = grade;
+        }
+    }
+}
