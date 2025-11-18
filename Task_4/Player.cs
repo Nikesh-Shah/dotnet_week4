2804e763294b29146738540a90f0648601f6f142
Nikesh Shah <156276029+Nikesh-Shah@users.noreply.github.com>
2025-11-18 10:37:26 +0545

    task_4 completed

---
+++ Task_4/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    public class Player
+    {
+        public string playerName;
+        public int level;
+        public string health;
+
+
+        public Player()
+        {
+            Console.WriteLine("Default Constructor Called");
+
+        }
+        public Player(string playerName, int level, string health)
+        {
+            Console.WriteLine("Parameterized Constructor Called");
+            this.playerName = playerName;
+            this.level = level;
+            this.health = health;
+
+        }
+
+    }
+}
---
+++ Task_4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_4
+{
+    public class Program 
+    {
+        public static void Main(string[] args)
+    {
+        Player player1 = new Player();
+            Console.WriteLine(player1.playerName);
+            Console.WriteLine(player1.level);
+            Console.WriteLine(player1.health);
+
+            Console.WriteLine("--------------");
+            Player player2 = new Player("Madan", 3, "Bad");
+            Console.WriteLine(player2.playerName);
+            Console.WriteLine(player2.level);
+            Console.WriteLine(player2.health);
+
+        }
+}
+    
+}
